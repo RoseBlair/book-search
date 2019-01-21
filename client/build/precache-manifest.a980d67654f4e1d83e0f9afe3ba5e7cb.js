d00fce1bcb7f3abc975d2a4152110499532a1dcd
carolyn <carolynbbernier@gmail.com>
2019-01-21 18:29:44 -0500

    mongo mongoose

---
+++ client/build/precache-manifest.a980d67654f4e1d83e0f9afe3ba5e7cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "495d93cc2c05774c4e88882137125113",
+    "url": "/static/media/logo.495d93cc.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1e2b1d577b1dccc4dcaf",
+    "url": "/static/js/main.1e2b1d57.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "1e2b1d577b1dccc4dcaf",
+    "url": "/static/css/main.72288d18.chunk.css"
+  },
+  {
+    "revision": "9d311f6016423cd55124ef9e28bfd878",
+    "url": "/index.html"
+  }
+];
